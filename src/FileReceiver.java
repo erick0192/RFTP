7bc5122379ffb1975218974302e14fdea14ca9bb
Erick <erick0192@hotmail.com>
2015-10-09 15:40:26 +0800

    First commit - empty

---
+++ src/FileReceiver.java
@@ -0,0 +1,8 @@
+
+public class FileReceiver {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ src/FileSender.java
@@ -0,0 +1,8 @@
+
+public class FileSender {
+
+	public static void main(String[] args) {
+
+	}
+
+}
